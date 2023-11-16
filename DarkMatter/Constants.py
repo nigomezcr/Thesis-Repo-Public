1b168e08e2fd345e922606403494959fb72f2ca2
nigomezcr <nigomezcr@unal.edu.co>
2023-11-16 08:41:30 -0500

    added Constants.py

---
+++ DarkMatter/Constants.py
@@ -0,0 +1,19 @@
+
+"""
+///////////// constants and Parameters ///////////////
+"""
+#Universal constants
+c = 3e+5 #Light speed in km/s
+G = 4.3e-3 # In units of pc/Ms * (km/s)^2
+h = 0.674 # Hubble constant in units of km/s/Mpc
+Mp = 1.22E19 # Planck mass in GeV
+
+#Masses
+me = 0.5e-3 # Electron Mass
+mm = 0.105 # Muon Mass
+
+#Conversion Factors
+rho_c = 2.7754e-7*h**2 # In units of Ms/Mpc-3
+GeVtocm2 = (1/5.06e13)**2 # cm^2
+GeVtog = (1/1.78e-24)
+fc =  GeVtocm2*GeVtog 
